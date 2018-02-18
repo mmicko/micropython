dab3cc4c1f9a742a17ad98f88166300828888514
Miodrag Milanovic <mmicko@gmail.com>
2018-02-18 15:16:38 +0100

    Make RAM bit larger

--- ports/lofive/main.c
+++ ports/lofive/main.c
@@ -44,7 +44,7 @@ void do_str(const char *src, mp_parse_input_kind_t input_kind) {
 }
 
 static char *stack_top;
-static char heap[2048];
+static char heap[1024*8];
 
 static const char sifive_msg[] = "\n\r\
 \n\r\
